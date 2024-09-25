04acb8ced31fa22caec5fcd669a23d44ba6fce85
Daniil Shcherbakov <Charel@bk.ru>
2024-09-25 22:12:43 +0300

    Service. Create first service

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -12,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
+
     }
 }
