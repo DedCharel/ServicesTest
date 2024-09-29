30e15476fb86606221d46566b892d2c8282a6ded
Daniil Shcherbakov <Charel@bk.ru>
2024-09-29 21:56:21 +0300

    Stop service

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
+           // stopService(MyForegroundService.newIntent(this)) //Остановка сервиса из вне
             startService(MyService.newIntent(this, 25))
         }
 
